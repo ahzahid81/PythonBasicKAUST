ad44064e3a00a5d0e77d2a0ac2d1dd2127761846
Abdul Hasib Zahid <abdulhasibzahid@gmail.com>
2024-10-23 00:03:36 +0300

    First Day's Task

---
+++ 2.6_TypeConversionFunction.py
@@ -0,0 +1,18 @@
+print(3.14, int(3.14))
+print(3.99999, int(3.99999))
+print(3.0, int(3.0))
+print(2345, int(2345))
+print(17, int(17))
+
+print(float(12.435))
+print(type(float(12.435)))
+
+print(str(17.5))
+print(str(420))
+print(type(str(533.5)))
+
+val = 100 + 5
+
+print("The value is " +str(val))
+
+print(type(float(int("5"))))
---
+++ 2.7_Variables.py
@@ -0,0 +1,20 @@
+message = "How are you?"
+pi = 3.14
+n = 7
+
+print(message)
+print(pi)
+print(n)
+
+x = 5
+print(x)
+
+x = 10
+print(x)
+
+day = "Thursday"
+day = 32.5
+day = 19
+print(day)
+
+A_good_grade_is_A+ = 5
