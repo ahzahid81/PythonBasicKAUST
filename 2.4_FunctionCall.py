dcd88250bae015add7f4963edce7fd5db1a5c4a3
Abdul Hasib Zahid <abdulhasibzahid@gmail.com>
2024-10-22 20:27:22 +0300

    first commit

---
+++ 2.4_FunctionCall.py
@@ -0,0 +1,12 @@
+def square(x):
+    return x*x
+
+def sub(a,b):
+    return a-b
+
+
+print(square(4))
+print(sub(6,4))
+
+print(square)
+print(square(3))
---
+++ 2.5_DataTypes.py
@@ -0,0 +1,31 @@
+print(type("Hello World!"))
+print(type(17))
+print("Hello World!")
+print(type(3.2))
+
+
+print(type(17))
+print(type(3.2))
+
+print(type("17"))
+print(type("3.2"))
+
+print(type('Abdul Hasib Zahid'))
+print(type("Abdul Hasib Zahid"))
+print(type('''Abdul Hasib Zahid'''))
+print(type("""Abdul Hasib Zahid"""))
+
+print("Bruch's beard")
+print('say "Hi"')
+
+print(42,500)
+
+print(45,46,3.11,5,0.0)
+print(10, "Zahid", 5)
+
+print("""This message will span
+several lines
+of the text.""")
+
+print('This is a string.')
+print("""And so is this.""")
